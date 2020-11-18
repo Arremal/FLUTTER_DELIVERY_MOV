f8be4d95047f2b5bd1feb4707df76acdaded6a3f
Joseph Santos <arremal90@gmail.com>
2020-11-18 08:35:25 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_delivery_mov/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_delivery_mov
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  @override
+  _InicioState createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mi App"),
+      ),
+      body: Center(
+        
+      ),
+    );
+  }
+}
